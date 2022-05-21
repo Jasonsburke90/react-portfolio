dd61766281b69ae4a8747bd59c8979a2124ffc72
unknown <jasonsburke90@gmail.com>
2022-05-21 14:49:22 -0500

    footer implemented

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,28 @@
 function Footer() {
-  return <footer> Here is my footer bar</footer>;
+  return (
+    <footer id="contact_me" className=" ai-c bgc-pine jc-sb p d-f@large">
+      <div className="fs-italic">Contact me</div>
+      <ul className="d-f@large ">
+        {" "}
+        <li class="ml@large">
+          <a href="mailto: jasonsburke90@gmail.com">Email Me</a>
+        </li>
+        <li class="ml@large">
+          <a href="./assets/docs/updatedresume.pdf" target="_blank">
+            Resume
+          </a>
+        </li>
+        <li class="ml@large">
+          <a href="https://www.linkedin.com/in/jason-burke-a29658223/">
+            Linkedin
+          </a>
+        </li>
+        <li class="ml@large">
+          <a href="https://github.com/Jasonsburke90">Github</a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer;
