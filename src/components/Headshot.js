f8b47ae50397f839e5d696bab4c9745a24485bea
unknown <jasonsburke90@gmail.com>
2022-05-21 14:46:05 -0500

    got headshot working

---
+++ src/components/Headshot.js
@@ -0,0 +1,10 @@
+import headshot from "../assets/images/jason-pic.jpg";
+function Headshot() {
+  return (
+    <div className="fx-1@lrg">
+      <img class="" src={headshot} alt="Jason Burke Headshot"></img>
+    </div>
+  );
+}
+
+export default Headshot;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,12 +1,25 @@
-function Header() {
+function NavigationBar() {
   return (
     <>
       {/* add flexbox */}
       <header>
-        <nav>About Me My Projects Contact Me</nav>
+        <nav className="ai-c d-f  p jc-sb@lrg">
+          <div className="bgc-pine fs-bold fs-italic fz-+++ p25 w-c">JB</div>
+          <ul className=" fz-+ p d-f@large">
+            <a>
+              <li className="ml@large">About Me </li>
+            </a>
+            <a>
+              <li className="ml@large"> My Projects </li>
+            </a>
+            <a>
+              <li className="ml@large">Contact Me </li>
+            </a>
+          </ul>
+        </nav>
       </header>
     </>
   );
 }
 
-export default Header;
+export default NavigationBar;
