becf637d148f69012efbab9e8eb66f735a625380
unknown <jasonsburke90@gmail.com>
2022-05-22 13:33:16 -0500

    work on contact forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,21 @@
 function Contact() {
   return (
     <>
-      <div id="about_me" className="fx-1@lrg fz-+@lrg">
-        <div>Enter your name below</div>
-        <form></form>
-        <div>Enter your email address below</div>
-        <form></form>
-        <div>Enter your message below</div>
-        <form></form>
+      <div id="" className="fx-1@lrg fz-+@lrg h-100">
+        <form>
+          <div className="m p">
+            <label className="m p">Enter your name here</label>
+            <input className="text-input" type="text"></input>
+          </div>
+          <div className="m p">
+            <label className="m p">Enter your email address here</label>
+            <input className="text-input" type="text"></input>
+          </div>
+          <div className="m p">
+            <label className="m p">Enter your message here</label>
+            <input className="text-input" type="text"></input>
+          </div>
+        </form>
       </div>
     </>
   );
